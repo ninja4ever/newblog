0dd8fb369c9499b7031d8a25c35b9a756918d44a
ninja4ever <evilninja@10g.pl>
2017-02-28 20:27:22 +0100

    change theme, install boostrap confirmation
    change theme, install boostrap confirmation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -83,5 +83,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/assets/bootstrapconfirmation/bootstrap-confirmation.min.js') }}"></script>
 </body>
 </html>
