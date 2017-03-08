4a9474447d48b13cc0ddd0d6ec7889cc66973167
ninja4ever <evilninja@10g.pl>
2017-03-08 22:16:35 +0100

    add active user status to login
    check active user status when user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -35,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    protected function authenticated(Request $request,User $user)
+    {
+      if($user->active == 0){
+          \Auth::logout();
+          \Session::flash('alert-warning', 'account deactivated');
+         return redirect('/login');
+      }
     }
 }
